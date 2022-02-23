425f23c339be4caa0efca40f39a1c6f04f806a3a
Jan Janech <jan.janech@kst.uniza.sk>
2022-02-23 13:09:01 +0100

    01a - atributy a gettery v triede Student

--- studenti/src/Student.java
+++ studenti/src/Student.java
@@ -4,6 +4,25 @@
  * @author janik
  */
 public class Student {
+    private final String meno;
+    private final String priezvisko;
+    private final String osobneCislo;
+
     public Student(String meno, String priezvisko, String osobneCislo) {
+        this.meno = meno;
+        this.priezvisko = priezvisko;
+        this.osobneCislo = osobneCislo;
+    }
+
+    public String getMeno() {
+        return this.meno;
+    }
+
+    public String getPriezvisko() {
+        return this.priezvisko;
+    }
+
+    public String getOsobneCislo() {
+        return this.osobneCislo;
     }
 }
