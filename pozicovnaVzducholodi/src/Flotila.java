bcca89981ec92d2bd49bd9ae48535410f1f1ecea
Jan Janech <jan.janech@kst.uniza.sk>
2022-02-24 17:08:39 +0100

    01b - vytvorenie vzducholode vo flotile

--- pozicovnaVzducholodi/src/Flotila.java
+++ pozicovnaVzducholodi/src/Flotila.java
@@ -1,11 +1,22 @@
+import java.util.ArrayList;
+
 /**
  * 24. 2. 2022 - 16:26
  *
  * @author janik
  */
 public class Flotila {
-    public void vytvorVzducholod(Konstrukcia konstrukcia, String nazov, int nosnost) {
+    private final ArrayList<Vzducholod> vzducholode;
+    private int posledneId;
 
+    public Flotila() {
+        this.vzducholode = new ArrayList<Vzducholod>();
+        this.posledneId = 0;
+    }
+
+    public void vytvorVzducholod(Konstrukcia konstrukcia, String nazov, int nosnost) {
+        this.posledneId++;
+        this.vzducholode.add(new Vzducholod(this.posledneId, konstrukcia, nazov, nosnost));
     }
 
     public Vzducholod najdiVzducholod(int nosnost, Konstrukcia konstrukcia, boolean ibaDostupna) {
