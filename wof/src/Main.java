25bf28ab8b42ec411208fa1288833af6af00ae82
Jan Janech <jan.janech@fri.uniza.sk>
2022-03-09 13:09:04 +0100

    03a - myslienky na dalsi rozvoj hry

--- wof/src/Main.java
+++ wof/src/Main.java
@@ -1,3 +1,30 @@
+/*
+ inventar
+ - predmety
+ - priklady:
+   - kluce na dvere
+   - navleky
+   - isic na dvere
+   - bagety (doplnenie hp)
+ miestnosti minaju hp
+ charakteristiky postavy
+ - body (level up = rocnik)
+ puzzle
+ kombinovanie predmetov
+   - crafting
+ mapa sveta
+ npc
+ - rozpravat
+   - davaju napovedy
+ - questy davaju
+ - predavaju predmety
+ - priklady:
+   - bufetarka predava bagety
+   - vratnicka dava napovedy
+ questy
+ - sprav skusku
+ - alt: uplatky
+*/
 /**
  * Hlavna trieda hry WoF s metodou main - spustanie v NB
  *
