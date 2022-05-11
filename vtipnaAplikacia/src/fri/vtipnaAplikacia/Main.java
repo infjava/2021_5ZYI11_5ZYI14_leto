8a2a45463091464296063652e0ab447e4a505cc6
Jan Janech <jan.janech@fri.uniza.sk>
2022-05-11 13:16:11 +0200

    12a - zmena temy na systemovu

--- vtipnaAplikacia/src/fri/vtipnaAplikacia/Main.java
+++ vtipnaAplikacia/src/fri/vtipnaAplikacia/Main.java
@@ -1,8 +1,16 @@
 package fri.vtipnaAplikacia;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
         VtipneOkno okno = new VtipneOkno();
         okno.zobraz();
     }
