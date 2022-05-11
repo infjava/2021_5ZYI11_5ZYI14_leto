6697eca584fc698b27d8fe0f3c111a68a82b38e2
Jan Janech <jan.janech@fri.uniza.sk>
2022-05-11 12:46:37 +0200

    12a - okno aplikacie

--- vtipnaAplikacia/src/fri/vtipnaAplikacia/Main.java
+++ vtipnaAplikacia/src/fri/vtipnaAplikacia/Main.java
@@ -1,8 +1,14 @@
 package fri.vtipnaAplikacia;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        JFrame okno = new JFrame("Vtipna aplikacia");
+        okno.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        okno.add(new JLabel("Chceš úspešne ukončiť predmet Informatika 2?"));
+        okno.pack();
+        okno.setVisible(true);
     }
 }
