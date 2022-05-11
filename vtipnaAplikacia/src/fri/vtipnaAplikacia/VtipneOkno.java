c0723154989459c8ca100d62c82cca5c75ef956c
Jan Janech <jan.janech@fri.uniza.sk>
2022-05-11 13:10:46 +0200

    12a - kombinacia layoutov

--- vtipnaAplikacia/src/fri/vtipnaAplikacia/VtipneOkno.java
+++ vtipnaAplikacia/src/fri/vtipnaAplikacia/VtipneOkno.java
@@ -10,8 +10,13 @@ public class VtipneOkno {
         this.okno = new JFrame("Vtipna aplikacia");
         this.okno.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.okno.add(new JLabel("Chceš úspešne ukončiť predmet Informatika 2?"), BorderLayout.NORTH);
-        this.okno.add(new JButton("Jasné"), BorderLayout.WEST);
-        this.okno.add(new JButton("Nie"), BorderLayout.EAST);
+
+        JPanel tlacitka = new JPanel();
+        tlacitka.setLayout(new GridLayout());
+        tlacitka.add(new JButton("Jasné"), BorderLayout.WEST);
+        tlacitka.add(new JButton("Nie"), BorderLayout.EAST);
+        this.okno.add(tlacitka, BorderLayout.CENTER);
+
         this.okno.pack();
     }
 
