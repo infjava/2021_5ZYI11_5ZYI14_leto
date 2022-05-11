89b8a8653952c73212accc2c6ce1a54a4fe8ef31
Jan Janech <jan.janech@fri.uniza.sk>
2022-05-11 13:08:24 +0200

    12a - pridane tlacitka

--- vtipnaAplikacia/src/fri/vtipnaAplikacia/VtipneOkno.java
+++ vtipnaAplikacia/src/fri/vtipnaAplikacia/VtipneOkno.java
@@ -1,6 +1,7 @@
 package fri.vtipnaAplikacia;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class VtipneOkno {
     private final JFrame okno;
@@ -8,7 +9,9 @@ public class VtipneOkno {
     public VtipneOkno() {
         this.okno = new JFrame("Vtipna aplikacia");
         this.okno.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.okno.add(new JLabel("Chceš úspešne ukončiť predmet Informatika 2?"));
+        this.okno.add(new JLabel("Chceš úspešne ukončiť predmet Informatika 2?"), BorderLayout.NORTH);
+        this.okno.add(new JButton("Jasné"), BorderLayout.WEST);
+        this.okno.add(new JButton("Nie"), BorderLayout.EAST);
         this.okno.pack();
     }
 
